2b7af321fc2fa9a0acd446f8f3f92d5126e68a73
nelsopet <peter.ross.nelson@gmail.com>
2021-05-17 20:41:35 -0400

    added shrubs to env output

--- Scripts/1_DataMunging.R
+++ Scripts/1_DataMunging.R
@@ -65,24 +65,31 @@ not_preds<-c("system:index"
 
 
 #Read in the old data
+
+#79 rows of env data with Tall, Midtall, Low and Dwarf shrubs
 GAAR_ENV<-read.csv(paste(wd,"/Data/Old/","GAAR_Plots.csv",sep=""), header=TRUE, sep=",", stringsAsFactors = FALSE)
-#79 rows of env data
+
+#446 rows of env data with shrub and subshrub
 XY_ENV<-read.csv(paste(wd,"/Data/Old/","XYEnvironmental_Data.csv", sep=""), sep=",", header=TRUE, stringsAsFactors = FALSE)
-#446 rows of env data
+
+#435 rows but no shrubs
 WAH_ENV<-read.table(paste(wd,"/Data/Old/","ENV_ARCN_Apr5_2017.txt", sep=""), header=TRUE, sep=",", stringsAsFactors = FALSE)
+
+#402 rows of env data but no shrubs. These are the data Emily used in the final manuscript on lichen depth 
 Full_Comm<-read.csv(paste(wd,"/Data/","full_community_14Jul20.csv", sep=""), sep=",", header=TRUE, stringsAsFactors = FALSE)
-#402 rows of env data
+
 Env<-read.table(paste(wd,"/Data/Old/","ENV_ARCN_June4_2019.txt", sep=""),sep=",", header=TRUE, stringsAsFactors = F) 
+
+#402 rows of env data but no shrubs. 
 Env<- Env %>% dplyr::select(plot, colnames(Full_Comm[,2:11]))
-#402 rows of env data
 
 #Find plots not in list of ARCN FIA plot used in the lichen depth analysis
 #WAH_ENV %>% dplyr::select(plot) %>% anti_join(GAAR_ENV, by=c("plot"="Veg_Plot")) %>% anti_join(XY_ENV, by="plot") %>% View()
-#Env %>% 
-#  dplyr::select(plot) %>% 
-#  anti_join(GAAR_ENV, by=c("plot"="Veg_Plot")) %>% 
-#  anti_join(XY_ENV, by="plot") %>% 
-#  View()
+Env %>%  # anti_join(XY_ENV, by="plot") 
+  dplyr::select(plot) %>% 
+  left_join(GAAR_ENV, by=c("plot"="Veg_Plot")) %>% 
+  left_join(XY_ENV, by="plot") %>% 
+  colnames()
 
 #Change the names of the plots to match those from GAAR and the rest of ARCN
 #WAH_ENV$plot<-ifelse(WAH_ENV$plot=="6L "       ,"6L",WAH_ENV$plot);
@@ -100,6 +107,33 @@ Env$plot<-ifelse(Env$plot=="DUNES1"    ,"Dunes1",Env$plot);
 Env$plot<-ifelse(Env$plot=="JLICHEN1"  ,"JLichen1",Env$plot);
 Env$plot<-ifelse(Env$plot=="JLICHEN2"  ,"JLichen2",Env$plot);
 Env$plot<-ifelse(Env$plot=="JMIXED1"   ,"JMixed1",Env$plot);
+
+XY_ENV$plot<-ifelse(XY_ENV$plot=="6L "       ,"6L",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="JPALISADE1","Jpalisad",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="DUNE2"     ,"Dune2",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="DUNES1"    ,"Dunes1",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="JLICHEN1"  ,"JLichen1",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="JLICHEN2"  ,"JLichen2",XY_ENV$plot);
+XY_ENV$plot<-ifelse(XY_ENV$plot=="JMIXED1"   ,"JMixed1",XY_ENV$plot);
+
+##Add shrubs
+
+Env_p1<-GAAR_ENV %>%
+  select(Veg_Plot,Tree:Water) %>% 
+  mutate(shrub = Tall_Shr+Mid_Tall+Low_Shru, 
+         subshrub = Dwarf_Sh) %>% 
+  select(-Tall_Shr:-Dwarf_Sh) %>% 
+  rename(plot = Veg_Plot, tree = Tree, gram = Graminoi, forb = Forbs, bryo = Bryophyt, lichen = Lichens, soil = Soil, duff = Duff, rock = Rock, water = Water, rock = Rock) %>% 
+  select(plot, sort(colnames(.))) #%>% colnames()
+  
+Env_p2<-  XY_ENV %>% 
+  select(plot,tree:water) %>% #View()
+  select(plot, sort(colnames(.)))# %>%
+    #colnames()
+
+Env_full<-rbind(Env_p1, Env_p2)
+Env_full<- Env %>% select(plot) %>% inner_join(Env_full)
+write_csv(Env_full,"./Output/env_data_for_lichen_volume_cover_by_color_group_NAD83.csv")
 
 ##Select the plots and needed columns from both the Gates and other ARCN parks
 #tmp1<-WAH_ENV %>% 
